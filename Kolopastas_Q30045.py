8da7e97d963e065279624b8f811dce7e120d9eaf
stavkol <stavkol@otenet.gr>
2024-12-16 13:48:59 +0200

    Add files via upload

---
+++ Kolopastas_Q30045.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 16 13:43:55 2024
+
+@author: user
+"""
+
+list1=["zero","one",2,"iii","100","V",6,"G",8,"ix"]
+length = len(list1)
+print(list1[0])
+print(list1[1])
+print(list1[2])
+print(list1[3])
+print(list1[4])
+print(list1[5])
+print(list1[6])
+print(list1[7])
+print(list1[8])
+print(list1[9])
+list1.append("A")
+print(list1)
+list1.remove("zero")
+print(list1)
+list1.remove("iii")
+print(list1)
+my_list2 = list1[1:4]
+print(my_list2)
+      
